5d7eb7c0a85171028831d3736fc16fc41f464d68
hoangnguyen-nfq <huuhoang.nguyen@nfq.asia>
2022-10-26 00:03:12 +0700

    Swith to 8080 port

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const __dirname = path.resolve();
 
 /** Get port from environment and store in Express. */
 // const port = process.PORT || '3000';
-const port = '3000';
+const port = '8080';
 app.set('port', port);
 
 app.use(logger('dev'));
